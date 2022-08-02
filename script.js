e083f644165168d4308f76ae314b673c4c52fa1a
Reks <39663719+BlackRexYT@users.noreply.github.com>
2022-08-02 19:38:40 +0300

    Uploaded replit repo

---
+++ script.js
@@ -0,0 +1,14 @@
+const images = [
+    {
+      expression: "happy",
+      images: [
+        "1.png", "2.png", "3.png", "4.png",
+      ],
+    },
+    {
+      expression: "Angry",
+      images: [
+        "1.png", "2.png", "3.png", "4.png",
+      ],
+    },
+  ]
